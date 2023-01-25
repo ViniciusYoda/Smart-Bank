e9372ff0cd1b2bfd03841bf8c276483149113a55
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-25 18:56:41 -0300

    estilizando o titulo

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+// import React from "react";
+import styled from 'styled-components'
 
-const Titulo = ({ children }) => {
+const Titulo = styled.h1`
+  color: grey;
+  padding: 25px 0
+
+`
+
+{/*const Titulo = ({ children }) => {
   return <h1 className="titulo">{children}</h1>;
-};
+}; */}
+
 export default Titulo;
