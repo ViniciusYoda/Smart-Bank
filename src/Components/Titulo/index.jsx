21790502538d9f683146934cd0eef34bbeae288b
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-26 18:40:46 -0300

    estilo do cabecalho

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -3,12 +3,8 @@ import styled from 'styled-components'
 
 const Titulo = styled.h1`
   color: grey;
-  padding: 25px 0
+  padding: 25px 0;
 
 `
-
-{/*const Titulo = ({ children }) => {
-  return <h1 className="titulo">{children}</h1>;
-}; */}
 
 export default Titulo;
